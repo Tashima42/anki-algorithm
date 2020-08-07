dee3ffdc510e9255506d333ba6789f56642f9bf2
Pedro Tashima <pedrotashima42@gmail.com>
2020-08-07 12:08:11 -0300

    feat: added module to convert akpg files to json

---
+++ ankiToJson.js
@@ -0,0 +1,3 @@
+const ankiToJson = require('anki-to-json').default
+
+ankiToJson('./data/anki/testdeck.apkg', 'data/anki-to-json/testdeck/')
--- readCards.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const readCardsJSON = (dir) => {
-    const readCardsFile = fs.readFileSync(dir)
-    const cardsObject = JSON.parse(readCardsFile)
-    return cardsObject
-}
-
-module.exports = {readCardsJSON : readCardsJSON}
